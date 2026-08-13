1f711cf4373d11647c626a308bb9eecdae424935
groobee-ci <groobee-ci@users.noreply.github.com>
2026-08-13 07:33:14 +0000

    Release 1.1.12 (prod)

--- GroobeeKit.xcframework/ios-arm64/GroobeeKit.framework/Headers/GroobeeKit-Swift.h
+++ GroobeeKit.xcframework/ios-arm64/GroobeeKit.framework/Headers/GroobeeKit-Swift.h
@@ -402,6 +402,8 @@ SWIFT_CLASS("_TtC10GroobeeKit7Groobee") SWIFT_AVAILABILITY(ios,introduced=10.0)
 - (void)setAppSessionEndWithIsTerminated:(BOOL)isTerminated iosSessionDtm:(NSString * _Nonnull)iosSessionDtm;
 - (void)setAppLocationInfoWithUserLocation:(CLLocation * _Nonnull)userLocation;
 - (NSDictionary<NSString *, NSString *> * _Nonnull)getGroobeeWebCookies SWIFT_WARN_UNUSED_RESULT;
+- (void)syncNativeToWeb:(NSString * _Nonnull)domain;
+- (void)syncWebToNativeWithWebView:(WKWebView * _Nonnull)webView urlRequest:(NSURLRequest * _Nonnull)urlRequest;
 - (void)setWebViewCookiesWithWebView:(WKWebView * _Nonnull)webView urlRequest:(NSURLRequest * _Nonnull)urlRequest;
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
